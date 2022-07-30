dd01c4b89ca22e42137e8473b17251c2e6d43e82
galifornia <martinho87@gmail.com>
2022-07-30 23:54:07 +0200

    [index] Hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,22 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+
+      <section className='max-w-7xl min-h-[40vh] mx-auto bg-yellow-500 flex justify-between items-center px-8 border-y-2 border-x-black'>
+        <div className='flex flex-col gap-4 w-full tablet:max-w-[50%]'>
+          <h1 className='text-3xl tablet:text-5xl font-serif'>
+            <span className='underline'>Medium</span> is a place to write, read
+            and, connect
+          </h1>
+          <p>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers
+          </p>
+        </div>
+        <div className='hidden phablet:block font-bold text-8xl tablet:text-[12rem] laptop:text-[18rem] desktop:text-[24rem] w-[50%] text-center font-serif'>
+          M
+        </div>
+      </section>
     </div>
   );
 };
