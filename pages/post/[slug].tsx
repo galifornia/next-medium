3822ecd0eaaa2882e6c44890c479f1a899e56640
galifornia <martinho87@gmail.com>
2022-08-01 23:57:17 +0200

    [posts] Add form for Comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import PortableText from 'react-portable-text';
 import Nav from '../../components/Nav';
 import { sanityClient, urlFor } from '../../config';
@@ -9,9 +10,25 @@ interface Props {
   post: PostType;
 }
 
+interface FormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
+
 const PostPage = ({ post }: Props) => {
   const mainImg = urlFor(post.mainImage).url();
   const authorImg = urlFor(post.author.image).url();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    console.log({ data });
+  };
   return (
     <main>
       <Nav />
@@ -32,6 +49,7 @@ const PostPage = ({ post }: Props) => {
         </div>
 
         <PortableText
+          className='mt-2'
           content={post.body}
           dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
           projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
@@ -53,6 +71,65 @@ const PostPage = ({ post }: Props) => {
           }}
         ></PortableText>
       </article>
+
+      <hr className='max-w-lg my-5 mx-auto border border-yellow-500' />
+      <div className='max-w-2xl mx-auto p-5'>
+        <h4 className='text-sm text-yellow-500'>Enjoyed this article?</h4>
+        <h5 className='text-3xl font-bold'>Leave a comment below!</h5>
+      </div>
+
+      <form
+        className='flex flex-col p-5 max-w-2xl mx-auto mb-10 gap-4 relative'
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input {...register('_id')} type='hidden' name='_id' value={post._id} />
+        <label className='flex flex-col gap-2'>
+          <span className='text-gray-700'>Name</span>
+          <input
+            {...register('name', { required: true })}
+            className='shadow border rounded form-input ring-yellow-500 outline-none focus:ring py-2 px-3'
+            placeholder='John Adams'
+            type='text'
+          />
+        </label>
+        <label className='flex flex-col gap-2'>
+          <span className='text-gray-700'>Email</span>
+          <input
+            {...register('email', { required: true })}
+            className='shadow border rounded form-input ring-yellow-500 outline-none focus:ring py-2 px-3'
+            placeholder='john.adams@gmail.com'
+            type='email'
+          />
+        </label>
+        <label className='flex flex-col gap-2'>
+          <span className='text-gray-700'>Comment</span>
+          <textarea
+            {...register('comment', { required: true })}
+            className='shadow border rounded form-textarea ring-yellow-500 outline-none focus:ring py-2 px-3'
+            placeholder='Write your comment here...'
+            rows={8}
+          />
+        </label>
+
+        <div className='flex flex-col gap-4'>
+          {errors.name && (
+            <span className='text-red-500'>- Name field is required</span>
+          )}
+          {errors.email && (
+            <span className='text-red-500'>- Email field is required</span>
+          )}
+          {errors.comment && (
+            <span className='text-red-500'>- Comment field is required</span>
+          )}
+        </div>
+
+        <button
+          className='bg-yellow-500 text-white font-bold w-full p-2 cursor-pointer '
+          type='submit'
+        >
+          Publish
+        </button>
+      </form>
     </main>
   );
 };
